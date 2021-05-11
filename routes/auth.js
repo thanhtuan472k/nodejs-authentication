bba61caad82d21036ff8d73586a3804052de9b34
Thanh Tuan <tuango.472000@gmail.com>
2021-05-11 22:07:46 +0700

    Authentication-project

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const {register,login, forgotPassword, resetPassword} = require('../controllers/authController');
+
+router.route('/register').post(register);
+
+router.route('/login').post(login);
+
+router.route('/forgotPassword').post(forgotPassword);
+
+router.route('/resetPassword/:resetToken').put(resetPassword);
+
+module.exports = router;
